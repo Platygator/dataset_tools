71e75860f14bf74176355cc74d9761dcd868a36c
Jan <j.schiff96@gmail.com>
2021-06-07 18:28:48 +0200

    param changes

--- plot_run.py
+++ plot_run.py
@@ -18,7 +18,7 @@ import glob
 
 label_folder = "results"
 data_folder = "mmseg_results"
-run_name = "crf_all"
+run_name = "real_1"
 
 # all tested parameter sets
 npy_name = f"{run_name}.npy"
@@ -30,12 +30,13 @@ ax.set(title=run_name,
 
 y_ticks = np.arange(0, 101, 10)
 ax.set_yticks(y_ticks)
-ax.set_ylim([20, 100])
+ax.set_ylim([0, 100])
 
 
 ax.grid(which='major', alpha=0.5)
 
-epochs = np.arange(10, 290, 10)
+epochs = np.arange(10, 610, 10)
+
 # plot IoU development
 set_data = np.load(f"{data_folder}/{npy_name}", allow_pickle=True).item()
 ax.plot(epochs, np.array(set_data['Background']) * 100, label='Background', color='blue')
@@ -44,13 +45,16 @@ ax.plot(epochs, np.array(set_data['Border']) * 100, label='Border', color='green
 ax.plot(epochs, np.array(set_data['Mean']) * 100, label='Mean', color='red')
 
 # plot IoU of label
-label_data = np.load(f"{label_folder}/{npy_name[:-4]}/result.npy", allow_pickle=True).item()
-ax.hlines(label_data['Background'] * 100, linestyles='dashed', xmin=10, xmax=280, color='blue')
-ax.hlines(label_data['Stone'] * 100, linestyles='dashed', xmin=10, xmax=280, color='orange')
-ax.hlines(label_data['Border'] * 100, linestyles='dashed', xmin=10, xmax=280, color='green')
-ax.hlines(label_data['Mean'] * 100, linestyles='dashed', xmin=10, xmax=280, color='red')
+try:
+    label_data = np.load(f"{label_folder}/{npy_name[:-4]}/result.npy", allow_pickle=True).item()
+    ax.hlines(label_data['Background'] * 100, linestyles='dashed', xmin=10, xmax=280, color='blue')
+    ax.hlines(label_data['Stone'] * 100, linestyles='dashed', xmin=10, xmax=280, color='orange')
+    ax.hlines(label_data['Border'] * 100, linestyles='dashed', xmin=10, xmax=280, color='green')
+    ax.hlines(label_data['Mean'] * 100, linestyles='dashed', xmin=10, xmax=280, color='red')
+except FileNotFoundError:
+    pass
 
 ax.legend(loc='best')
 # plt.show()
 
-fig.savefig(f"results/plots/{npy_name}.png")
+fig.savefig(f"results/plots/{run_name}.png")
